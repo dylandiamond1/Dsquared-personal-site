560de5b3157a1e993df18f6825e8e3dbef164462
dylandiamond <dylandiamond34@gmail.com>
2022-08-15 11:28:59 -0400

    finished website

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const h2 = document.createElement("h2");
 h2.textContent = "Click Each Character!";
 document.querySelector("body").appendChild(h2);
 
+// click event for "The Sopranos"
 const input = document.getElementById('test');
 input.addEventListener('click', function() {
   alert('The best show there ever was!');
 });
 
-
+//button
 const button = document.getElementById("test")
-
 function changeColor() {
     button.style.color = "red"
 }
